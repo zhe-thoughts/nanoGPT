5819d1b59f4bb617a3b2f91578f4645dedbb5615
Zhe Zhang <zhezh@zhezh-mlt.client.nvidia.com>
2024-09-09 16:15:09 -0700

    What if synchronously to.device?

--- train.py
+++ train.py
@@ -126,7 +126,8 @@ def get_batch(split):
     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
     if device_type == 'cuda':
         # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
-        x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+        # x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+        x, y = x.pin_memory().to(device, non_blocking=False), y.pin_memory().to(device, non_blocking=False)
     else:
         x, y = x.to(device), y.to(device)
 
